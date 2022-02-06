196c5999a4584a6039f94dd56f7208fe7dc8647a
louarhou <l.ouarhou.ensa@.com>
2022-02-06 23:40:03 +0100

    feat(typeorm): add and configure typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
+import { UsersModule } from './modules/user/user.module';
 import { CacheModule, Module } from '@nestjs/common';
 import { PostsModule } from './modules/posts/posts.module';
 import { PingModule } from './modules/ping/ping.module';
 import { CommonModule } from './common/common.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getConnectionOptions } from 'typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          autoLoadEntities: true,
+        }),
+    }),
     PostsModule,
     PingModule,
+    UsersModule,
     CommonModule,
     CacheModule.register({
       ttl: 0,
